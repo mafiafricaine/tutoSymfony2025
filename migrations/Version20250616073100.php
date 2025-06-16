27613b4ae5279231aa0e35f407508e375010b8a8
julienDunia <juliendunia@hotmail.com>
2025-06-16 10:15:04 +0200

    Rajout dans la page index et show de recipe, le nom et prénom de l'utilisateur

--- migrations/Version20250616073100.php
+++ migrations/Version20250616073100.php
@@ -10,24 +10,21 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250613092706 extends AbstractMigration
+final class Version20250616073100 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Ajout de la relation entre User et Recipe';
+        return 'On a du effacer la table recipes pour pouvoir refaire la migration sans données présentes';
     }
 
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            ALTER TABLE recipes ADD user_id INT NOT NULL
+            CREATE TABLE recipes (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, title VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, duration INT DEFAULT NULL, image_name VARCHAR(500) DEFAULT NULL, created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', updated_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', UNIQUE INDEX UNIQ_A369E2B52B36786B (title), INDEX IDX_A369E2B5A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
         $this->addSql(<<<'SQL'
             ALTER TABLE recipes ADD CONSTRAINT FK_A369E2B5A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)
-        SQL);
-        $this->addSql(<<<'SQL'
-            CREATE INDEX IDX_A369E2B5A76ED395 ON recipes (user_id)
         SQL);
     }
 
@@ -38,10 +35,7 @@ final class Version20250613092706 extends AbstractMigration
             ALTER TABLE recipes DROP FOREIGN KEY FK_A369E2B5A76ED395
         SQL);
         $this->addSql(<<<'SQL'
-            DROP INDEX IDX_A369E2B5A76ED395 ON recipes
-        SQL);
-        $this->addSql(<<<'SQL'
-            ALTER TABLE recipes DROP user_id
+            DROP TABLE recipes
         SQL);
     }
 }
